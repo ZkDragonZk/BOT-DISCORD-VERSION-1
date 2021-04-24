b27db03bd1050a9a9481920549530f3091713305
ZkDragonZk <75184968+ZkDragonZk@users.noreply.github.com>
2021-04-24 09:47:43 -0300

    Add files via upload

---
+++ events/client/ready.js
@@ -0,0 +1,20 @@
+module.exports = client => {
+  let botStatus = [
+    `${client.guilds.cache.size} servers!`,
+    "-help",
+    `Total ${client.users.cache.size} users!`,
+    `Total ${client.channels.cache.size} channels!`
+]
+
+    setInterval(function() {
+    let status = botStatus[Math.floor(Math.random() * botStatus.length)];
+    client.user.setActivity(status, {type: "WATCHING"});
+
+    }, 5000)
+
+    client.user.setUsername('Sorn'); // sets the bots name
+    client.user.setStatus("online"); // sets the bots status
+    
+  console.log(`Hello ${client.user.username} is now online!`); // consoles logs this when bot is turned on
+   
+};
